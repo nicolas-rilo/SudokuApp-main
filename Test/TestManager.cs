0e705e3ce5f870e7b6da8f651ca78b4aff9351ed
Nicolas Rilo Botana <nicolas.rilo@udc.es>
2024-04-23 21:35:38 +0200

    Importar de otro namespace correcto

---
+++ Test/TestManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Es.Udc.DotNet.SudokuApp.Model.UserService;
+using Ninject;
+
+namespace Es.Udc.DotNet.SudokuApp.Test
+{
+    public class TestManager
+    {
+        private static IKernel kernel;
+        private static IUserService userService;
+
+    }
+}
