2e6b584be426c0556dbca0db7671751702f7f296
Nicolas Rilo Botana <nicolas.rilo@udc.es>
2024-04-27 18:20:15 +0200

    Sudoku dao

---
+++ SudokuApp/SudokuDao/ISudokuDao.cs
@@ -0,0 +1,17 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using Es.Udc.DotNet.SudokuApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.SudokuApp.Model.SudokuDao
+{
+    public interface ISudokuDao : IGenericDao<Sudoku, Int64>
+    {
+        List<Sudoku> findByUser(Users user, int start, int size);
+
+        List<Sudoku> findByFilter(string name, string dificulty,bool killer, bool thermal, bool arrow, bool custom, int start, int size);
+    }
+}
---
+++ SudokuApp/SudokuDao/SudokuDaoEntityFramework.cs
@@ -0,0 +1,49 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.SudokuApp.Model;
+
+namespace Es.Udc.DotNet.SudokuApp.Model.SudokuDao
+{
+    class SudokuDaoEntityFramework : GenericDaoEntityFramework<Sudoku, Int64>, ISudokuDao
+    {
+        public List<Sudoku> findByFilter(string name, string dificulty, bool killer, bool thermal, bool arrow, bool custom, int start, int size)
+        {
+
+            DbSet<Sudoku> sudokus = Context.Set<Sudoku>();
+
+            var result = from s in sudokus select s;
+
+            if (name != null) {
+                name = name.ToLower();
+                result = result.Where(i => i.name.Contains(name));
+            }
+
+            if (dificulty != null) { 
+                dificulty = dificulty.ToLower();
+                result = result.Where(i => i.dificulty == dificulty);
+            }
+
+            result =  killer ? result.Where(i => i.killer == true) : result;
+
+            result = thermal ? result.Where(i => i.thermal == true) : result;
+
+            result = arrow ? result.Where(i => i.arrow == true) : result;
+
+            result = custom ? result.Where(i => i.custom == true) : result;
+
+            return result.Skip(start).Take(size).ToList();
+
+        }
+
+        public List<Sudoku> findByUser(Users user, int start, int size)
+        {
+            return user.Sudoku.Skip(start).Take(size).ToList();
+        }
+    }
+}
